367ab903bc149984b4bfad582d529c7c7823fc42
pab-lo-hub <salaberry.pablo@gmail.com>
2023-01-26 14:43:09 -0300

    new modules and forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
-import { MainPageComponent } from './star-wars/main-page/main-page.component';
 import { StarWarsModule } from './star-wars/star-wars.module';
 
 @NgModule({
--- src/app/star-wars/main-page/main-page.component.ts
+++ src/app/star-wars/main-page/main-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Skywalker Luke',
+    poder: 20000,
+  }
 
+  cambiarNombre( event: any ) {
+    console.log(event.target.value);
+  }
+
+  agregar() {
+    console.log(this.nuevo);
+  }
 }
