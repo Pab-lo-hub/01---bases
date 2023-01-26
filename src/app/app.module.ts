0bd83b0648627d29243fa047c2046226d30a005e
pab-lo-hub <salaberry.pablo@gmail.com>
2023-01-26 13:34:21 -0300

    adding modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { MainPageComponent } from './star-wars/main-page/main-page.component';
+import { StarWarsModule } from './star-wars/star-wars.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { ContadorModule } from './contador/contador.module';
     BrowserModule,
     HeroesModule,
     ContadorModule,
+    StarWarsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
